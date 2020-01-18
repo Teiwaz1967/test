aaab5ebfa3f1487755f4b46f579a18619c41e2db
Teiwaz <region76teiwaz@gmail.com>
2020-01-18 16:58:20 +0300

    First task

--- script001.js
+++ script001.js
@@ -1,5 +1,17 @@
-var string = "HELLO, WORLD!!!\nHELLO, WORLD!!!";
-console.log(string);
-alert(string);
-
+var money = prompt("What is your budget for a month ?");
+var time = prompt("Enter a date in the format YYYY-MM-DD");
+var answerFirstQuestion = prompt("Enter a required cost item this month");
+var answerSecondQuestion = prompt("How much will it cost ?");
+var dailyBudget = (money - answerSecondQuestion) / 30;
+var appData = {
+    "budget": money,
+    "timeData": time,
+    "expenses": {
+        "answerFirstQuestion": answerSecondQuestion
+    },
+    "optionalExpenses": {},
+    "income": [],
+    "savings": false
+};
+alert("Your daily budget " + dailyBudget);
 
